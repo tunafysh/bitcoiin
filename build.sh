2cf68bf9e4f8357d75bfe90cede7403dfa54f062
tunafysh <hanansmani@gmail.com>
2025-01-16 13:22:26 +0100

    idk

--- build.sh
+++ build.sh
@@ -1,42 +1,17 @@
-#!/bin/bash
-
-printf "\e[1;36mSetting up...\n"
-export DEVKITPRO=/opt/devkitpro
-export DEVKITARM=${DEVKITPRO}/devkitARM
-export DEVKITPPC=${DEVKITPRO}/devkitPPC
-export PATH=${DEVKITPRO}/tools/bin:$PATH
-export PATH=$PATH:${DEVKITPRO}/devkitPPC/bin
-printf "\e[1;32mDone.\n"
-
-0printf "\e[1;36mCompiling...\n"
-cargo +nightly build -Zbuild-std=core,alloc --target powerpc-unknown-eabi.json
-
+#!/bin/sh
+echo $DEVKITPRO
+command -v elf2dol > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    printf "\e[1;31mError in target\n"
-    exit 1
-else 
-    printf "\e[1;32mDone.\n"
+        printf "\e[31mError: elf2dol is not installed.\e[0m"
 fi
-
-0printf "\e[1;36mCopying file and creating Homebrew app...\n"
-if ! command -v elf2dol &> /dev/null; then
-    printf "\e[1;31melf2dol could not be found. Please install it.\n"
-    exit 1
+export DEVKITPRO=/opt/devkitpro
+export PATH=$PATH:${DEVKITPRO}/devkitPPC/bin
+export PATH=${DEVKITPRO}/tools/bin:$PATH
+if [ -f boot.elf ] && [ -f boot.dol ]; then
+    rm boot.elf boot.dol
 fi
-
-elf2dol target/powerpc-unknown-eabi/debug/bitcoiin boot.dol
-
-if [[ -d build ]]; then
-    rm -rf build
+cargo +nightly build -Zbuild-std=core,alloc --target powerpc-unknown-eabi.json
+if [ $? -eq 0 ]; then
+    cp target/powerpc-unknown-eabi/debug/indigo boot.elf
+    elf2dol boot.elf boot.dol
 fi
-
-mkdir -p build/apps/bitcoiin
-cp boot.dol build/apps/bitcoiin/boot.dol
-cp assets/meta.xml build/apps/bitcoiin/meta.xml
-cp assets/icon.png build/apps/bitcoiin/icon.png
-cd build || exit
-zip -r bitcoiin.zip apps
-cp bitcoiin.zip ../bitcoiin.zip
-cd ..
-rm -rf build
-printf "\e[1;32mDone.\n"
--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,39 @@
 #![no_std]
 #![feature(start)]
 
-mod display;
-use crate::display::Display;
-
 extern crate alloc;
-
-use embedded_graphics::{
-    image::Image, mono_font::{ascii::FONT_6X10, MonoTextStyle}, pixelcolor::Rgb888, prelude::*, primitives::{PrimitiveStyle, Rectangle}, text::Text, Drawable
-};
-
 use ogc_rs::prelude::*;
+use ogc_rs::println;
 
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    let curdata = include_bytes!("../assets/Wii-Cursor.tga");
+    // Initialise the video system
+    let video = Video::init();
 
-    let curtga = Tga::from_slice(curdata).unwrap();
-    let curimage =Image::new(&curtga, Point::zero());
-
-    let mut video = Video::init();
-    Input::init(ControllerType::Gamecube);
-    Input::init(ControllerType::Wii);
-
-    let gcn_ctrl = Input::new(ControllerType::Gamecube, ControllerPort::One);
-    let wii_ctrl = (0..4)
-        .filter_map(|port| Input::new(ControllerType::Wii, ControllerPort::from(port)).ok())
-        .find(|ctrl| ctrl.is_connected())
-        .expect("No Wii controller connected");
-    wii_ctrl
-        .as_wpad()
-        .set_data_format(WPadDataFormat::ButtonsAccelIR);
-
+    // Initialise the console, required for print.
     Console::init(&video);
-    Video::configure(&video.render_config);
+
+    // Set up the video registers with the chosen mode.
+    Video::configure((&video.render_config).into());
+
+    // Tell the video hardware where our display memory is.
     unsafe {
         Video::set_next_framebuffer(video.framebuffer);
     }
-    Video::set_black(true);
-    Video::flush();
-    Video::wait_vsync();
 
-    let mut wii_display = Display::new(256 * 1024);
-    wii_display.setup(&mut video.render_config);
+    // Make the display visible.
     Video::set_black(false);
 
-    println!("Hello World!");
+    // Flush the video register changes to the hardware.
+    Video::flush();
 
-    const BACKGROUND: Rectangle = Rectangle::new(Point::zero(), Size::new(640, 528));
+    // Wait for Video setup to complete.
+    Video::wait_vsync();
 
-    let style = MonoTextStyle::new(&FONT_6X10, Rgb888::CSS_LIGHT_BLUE);
+    println!("Hello, World!");
 
-    const CENTER: Point = Point::new(640 / 2, 528 / 2);
-    let area: Rectangle = Rectangle::with_center(CENTER, Size::new(200, 200));
-
-    const POINTER: Rectangle = Rectangle::new(Point::zero(), Size::new_equal(10));
     loop {
-        Input::update(ControllerType::Gamecube);
-        Input::update(ControllerType::Wii);
-
-        if gcn_ctrl.is_button_down(Button::Start) {
-            break 0;
-        }
-
-        if wii_ctrl.is_button_down(Button::Home) {
-            break 0;
-        }
-        Gx::set_viewport(
-            0.0,
-            0.0,
-            video.render_config.framebuffer_width as f32,
-            video.render_config.embed_framebuffer_height as f32,
-            0.0,
-            0.0,
-        );
-
-        let ir = Point::new(
-            wii_ctrl.as_wpad().ir().0 as i32,
-            wii_ctrl.as_wpad().ir().1 as i32,
-        );
-
-        BACKGROUND
-            .into_styled(PrimitiveStyle::with_fill(Rgb888::WHITE))
-            .draw(&mut wii_display)
-            .unwrap();
-
-        if POINTER.translate(ir).intersection(&area).size != Size::zero() {
-            area.into_styled(PrimitiveStyle::with_fill(Rgb888::RED))
-                .draw(&mut wii_display)
-                .unwrap();
-            if wii_ctrl.is_button_held(Button::A) {
-                area.into_styled(PrimitiveStyle::with_fill(Rgb888::GREEN))
-                    .draw(&mut wii_display)
-                    .unwrap();
-            }
-        } else {
-            area.into_styled(PrimitiveStyle::with_fill(Rgb888::BLUE))
-                .draw(&mut wii_display)
-                .unwrap();
-        }
-
-        POINTER
-            .translate(ir)
-            .into_styled(PrimitiveStyle::with_fill(Rgb888::CYAN))
-            .draw(&mut wii_display)
-            .unwrap();
-
-        Text::new("Hello Rust!", Point::new(20, 30), style).draw(&mut Display).unwrap();
-
-        wii_display.flush(video.framebuffer);
-
-        unsafe {
-            Video::set_next_framebuffer(video.framebuffer);
-        }
-        Video::flush();
+        // Wait for the next frame.
         Video::wait_vsync();
     }
 }
