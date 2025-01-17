0e01b97bb5c33f883851a3e2d3068f3e4572b50d
tunafysh <hanansmani@gmail.com>
2025-01-17 10:46:31 +0100

    added C

--- rust/build.sh
+++ rust/build.sh
@@ -4,22 +4,24 @@ command -v elf2dol > /dev/null 2>&1
 if [ $? -ne 0 ]; then
         printf "\e[31mError: elf2dol is not installed.\e[0m"
 fi
-mode="release"
-
-if [ "$1" = "-d" ]; then
-
-    mode="debug"
-
-fi
 
 export DEVKITPRO=/opt/devkitpro
 export PATH=$PATH:${DEVKITPRO}/devkitPPC/bin
 export PATH=${DEVKITPRO}/tools/bin:$PATH
+
+mode="release"
+
+if [ "$1" = "-d" ]; then
+    mode="debug"
+fi
+
 if [ -f boot.elf ] && [ -f boot.dol ]; then
     rm boot.elf boot.dol
 fi
+cd rust
 cargo +nightly build -Zbuild-std=core,alloc --target powerpc-unknown-eabi.json --$mode
+cd ..
 if [ $? -eq 0 ]; then
-    cp target/powerpc-unknown-eabi/$mode/indigo boot.elf
+    cp rust/target/powerpc-unknown-eabi/$mode/indigo boot.elf
     elf2dol boot.elf boot.dol
 fi
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,24 +3,41 @@
 
 extern crate alloc;
 use ogc_rs::prelude::*;
+use ogc_rs::network::*;
 
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
+
     let video = Video::init();
+
     Input::init(ControllerType::Gamecube);
     Input::init(ControllerType::Wii);
+
     let gcn_ctrl = Input::new(ControllerType::Gamecube, ControllerPort::One);
     let wii_ctrl = Input::new(ControllerType::Wii, ControllerPort::One);
+
     Console::init(&video);
+
     Video::configure(&video.render_config);
+
     unsafe {
         Video::set_next_framebuffer(video.framebuffer);
     }
+
     Video::set_black(false);
     Video::flush();
     Video::wait_vsync();
 
-    println!("Hello World!");
+    println!("\033[1;36mHello World!");
+    
+    let net = Network::init().expect("Failed to initialize network");
+    let sock = Network::new_socket(ProtocolFamily::AfInet, SocketType::SockStream).expect("Failed to create socket");
+    let addr = IPV4Address {
+        address: 0xC0A8B2F4,
+    };
+    
+    sock.bind(addr, 80).expect("Failed to bind socket");
+    sock.listen(5).expect("Failed to listen on socket");
 
    loop {
         Input::update(ControllerType::Gamecube);
